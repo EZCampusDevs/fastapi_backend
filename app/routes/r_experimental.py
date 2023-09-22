95c79324700d590d2083eec1efb6f80e3dcc241c
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2023-09-21 22:25:17 -0400

    Fix documentation

--- app/routes/r_experimental.py
+++ app/routes/r_experimental.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-""""ICS download API endpoint routes."""
+""""Experimental endpoints."""
 
 from fastapi import APIRouter, HTTPException, Request, status
 from pydantic import BaseModel
